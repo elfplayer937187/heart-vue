bdab9590f89b20fff73a43579313e6737a18f1fe
elfplayer937187 <elfplayer937187@gmail.com>
2026-08-30 17:30:24 +0800

    fix: 修复接口多/api的bug

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,15 +1,16 @@
 import axios from 'axios'
 import { ElMessage } from 'element-plus'
+import { getToken } from './token'
 // 创建请求实例
 const request = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_URL,
   timeout: 5000,
 })
 // 请求拦截器
 request.interceptors.request.use(
   (config) => {
     // 在发送请求之前做些什么
-    const token = localStorage.getItem('token')
+    const token = getToken()
     if (token) {
       config.headers['token'] = token
     }
@@ -24,30 +25,35 @@ request.interceptors.request.use(
 // 响应拦截器
 request.interceptors.response.use(
   (response) => {
-    // 对响应数据做点什么
-    const { data, config } = response
-    // 处理业务状态码
-    if (data.code === '200') {
-      return data.data
-    } else {
-      if (data.code === '-1') {
-        if (!config.url?.includes('/login')) {
-          ElMessage.error(data.msg || '登录过期，请重新登录')
-
-          // 清除登录信息
-          localStorage.removeItem('token')
-          localStorage.removeItem('userInfo')
-          window.location.href = '/auth/login'
-        } else {
-          ElMessage.error(data.msg || '登录过期，请重新登录')
-          return Promise.reject('网络请求失败....')
-        }
-      }
-    }
     return response.data
   },
   (error) => {
-    // 对响应错误做点什么
+    //处理网络错误
+    let msg = ''
+    const status = error.response.status
+    switch (status) {
+      case 400:
+        msg = '请求参数错误'
+        break
+      case 401:
+        msg = 'token过期'
+        break
+      case 403:
+        msg = '无权访问'
+        break
+      case 404:
+        msg = '请求地址错误'
+        break
+      case 500:
+        msg = '服务器出现问题'
+        break
+      default:
+        msg = '无网络'
+    }
+    ElMessage({
+      type: 'error',
+      message: msg,
+    })
     return Promise.reject(error)
   },
 )
